ea92bd3a2bb12fa45c49a89eeb0cba445559c998
Dhiviya <dhiviarul@gmail.com>
2015-12-22 08:46:34 +0530

    Home about experience and news pages are committed

--- wp-content/themes/christophe/footer.php
+++ wp-content/themes/christophe/footer.php
@@ -1,9 +1,18 @@
-</div><!-- .site-content -->
-
+<div class="clearfix"></div>
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="container">
+			<div class="footer-copyright">
+				<span>&copy; 2015</span>
+				<span>CHRISTOPHE BUECHER</span>
+			</div>
+			<div class="footer-social">
+			<span><img src="../wp-content/themes/christophe/images/linkedin.png"></img></span>
+			<span><img src="../wp-content/themes/christophe/images/linkedin.png"></img></span>
+			<span><img src="../wp-content/themes/christophe/images/linkedin.png"></img></span>		
+			</div>
+		</div>
 	</footer>
 
-<?php wp_footer(); ?>
 
 </body>
 </html>
