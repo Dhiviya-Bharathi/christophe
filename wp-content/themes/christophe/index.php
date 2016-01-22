419beabee83dcc60dc20ddfce371e398f809d75f
nijanthanj <nijanthanj35ece@gmail.com>
2016-01-23 00:14:28 +0530

    footer and ios device fixes

--- wp-content/themes/christophe/index.php
+++ wp-content/themes/christophe/index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <?php wp_head(); ?>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="wp-content/themes/christophe/css/bootstrap.min.css"/>
 <link rel="stylesheet" href="wp-content/themes/christophe/css/font-awesome.min.css"/>
 <link rel="stylesheet" href="wp-content/themes/christophe/style.css"/>
